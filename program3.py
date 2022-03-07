c8ef49982b1dcc238c34da13342497d93dee1be1
Dasiripalli Joshna <91077542+DasiripalliJoshna@users.noreply.github.com>
2022-03-07 19:53:27 +0530

    Create program3.py

---
+++ program3.py
@@ -0,0 +1,10 @@
+# program to find the palindrome or not
+def isPalindrome(Input_String):
+    Reverse_String = Input_String[::-1]
+    return Input_String == Reverse_String
+
+
+if isPalindrome(input("Enter your string-")):
+    print("The string is a palindrome")
+else:
+    print("The string is NOT a palindrome")
