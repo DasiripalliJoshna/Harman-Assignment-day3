c6b8ebfc4c1e3a46c60b2928a36387d59472c536
Dasiripalli Joshna <91077542+DasiripalliJoshna@users.noreply.github.com>
2022-03-07 19:59:46 +0530

    Update program2.py

--- program2.py
+++ program2.py
@@ -1,5 +1,5 @@
 # Write a program to find len os string input from user and save the length to the file data.txt.
-file = open("String_length.txt", 'a')
+file = open("data.txt", 'a')
 file.write("\n")
 User_Input = input("Enter the String- ")
 data = User_Input+","+str(len(User_Input))
